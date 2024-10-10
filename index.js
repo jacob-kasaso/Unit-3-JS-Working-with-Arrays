baa21ced95d81464611d356b046cabd0eb2e85a0
jacob-kasaso <jacobkasaso@gmail.com>
2024-10-10 21:20:05 +0300

    arrays.practise

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+      cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+
+function appendCat(name){
+    return [name,...cats]
+}
+function prependCat(name){
+    return [name,...cats]
+}
+function removeLastCat(){
+    return cats.slice(0,cats.length - 1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function appendCat(name){
+    return [name,...cats]
+}
+function appendCat(name){
+    let newVar = "Ralph"
+    let newArray = cats.concat(newVar)
+}
+function appendCat(name){
+    return [name,...cats]
+}
+function appendCat(name){
+    cats.pop(name)
+}
+function removeLastCat(name){
+    return cats.slice(0,cats.length - 1)
+}
